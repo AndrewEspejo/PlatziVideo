7594c8e5579c49d50ee71fb46619c4f58ef05436
ANdres espejo <jeaespejogo@unal.edu.co>
2020-07-29 02:39:28 -0500

    Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,22 +5,24 @@ import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
 
 const App = () => (
     <div className="App">
         <Header />
         <Search />
-		<Categories>
-			<Carousel>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-			</Carousel>
-		</Categories>
+        <Categories>
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Footer />
     </div>
 );
 
