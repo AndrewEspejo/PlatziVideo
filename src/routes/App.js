a5f930dbc9e2afe2790a1ae8c9eae460fb6f9f60
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-07 15:20:31 -0500

    Register

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Home from "../containers/Home";
 import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 const App = () => (
     <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Login" component={Login} />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Register" component={Register} />
+        </Switch>
     </BrowserRouter>
 );
 
