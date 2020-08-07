431a568eb8b43e49bdb53387fd3f171d612da9c9
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-07 14:59:15 -0500

    Login

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import Home from "../containers/Home";
+import Login from "../containers/Login";
 
 const App = () => (
     <BrowserRouter>
         <Route exact path="/" component={Home} />
+        <Route exact path="/Login" component={Login} />
     </BrowserRouter>
 );
 
