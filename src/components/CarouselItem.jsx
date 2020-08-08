9d60e8b8fa3f87a8392a28282fb7bc42988bee85
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-08 02:28:20 -0500

    correcion id

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -7,9 +7,9 @@ import plusIcon from "../assets/static/plus-icon.png";
 import { setFavorite } from "../actions";
 
 const CarouselItem = (props) => {
-    const { cover, title, year, contentRating, duration } = props;
+    const { id, cover, title, year, contentRating, duration } = props;
     const handleSetFavorite = () => {
-        props.setFavorite({ cover, title, year, contentRating, duration });
+        props.setFavorite({ id, cover, title, year, contentRating, duration });
     };
     return (
         <div className="carousel-item">
